934c21c1dbe468cbf8668a3dbe474e3725d3ba6b
vikrampalkamboj <vikramvinayak24@gmail.com>
2018-05-06 01:29:22 -0400

    Creating a custom calender or using a calendar for a specifc country in a dataset

--- data_processing_datetimeindex.py
+++ data_processing_datetimeindex.py
@@ -23,8 +23,51 @@ sf.plot()
 
 rng=pd.date_range(start='6/1/2017', end='6/30/2017', freq='B')
 
-#setting up new argument
+#setting up new index argument
 df.set_index(rng, inplace=True)
+
+
+st=df.Close
+st.plot()
+
+
+#Generating frequency on missing days or weekends, it will carry over the last
+#date figure
+
+pt=st.asfreq('D', method='pad')
+
+
+#When you only know about starting date, freq='H'
+
+rng=pd.date_range(start='6/1/2017', periods=72, freq='B')
+
+
+# it is useful when we have to do unit, we can generate fake dataset using this
+ 
+import numpy as np
+ts=pd.Series(np.random.randint(1,10,len(rng)), index=rng)
+
+
+# using custom calendar (US calendar)
+
+from pandas.tseries.holiday import USFederalHolidayCalendar
+from pandas.tseries.offsets import CustomBusinessDay
+
+usb=CustomBusinessDay(calender=USFederalHolidayCalendar())
+
+
+# Creating a specific calendar
+from pandas.tseries.holiday import  AbstractHolidayCalendar, nearest_workday, Holiday
+class myBirthdayCalendar(AbstractHolidayCalendar):
+    rules = [
+        Holiday('Vikram pal Birthday', month=9, day=12)
+    ]
+
+
+myc=CustomBusinessDay(calendar=myBirthdayCalendar())
+
+
+
 
 
 
