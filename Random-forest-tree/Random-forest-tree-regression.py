342000f5eb98ad6deb9dd866e8f18e7dd6aad771
vikrampalkamboj <vikramvinayak24@gmail.com>
2018-05-16 01:47:53 -0400

    Random forest Regression

--- Random-forest-tree/Random-forest-tree-regression.py
+++ Random-forest-tree/Random-forest-tree-regression.py
@@ -1,7 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Data Preprocessing Template
-
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,17 +10,14 @@ X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
 
-from sklearn.tree import DecisionTreeRegressor
-regressor=DecisionTreeRegressor(random_state=0)
+
+from sklearn.ensemble import RandomForestRegressor
+regressor=RandomForestRegressor(n_estimators=300, random_state=0)
 regressor.fit(X,y)
-
-
-#Predicting a new result with Polynomial resgression
-y_predict=regressor.predict(6.5)
 
 plt.scatter(X,y, color='red')
 plt.plot(X, regressor.predict(X), color='blue')
-plt.title("Truth or Bluff (Polynomial Regression)")
+plt.title("Truth or Bluff (Random Forest Regression)")
 plt.xlabel('Position level')
 plt.ylabel("Salary")
 plt.show()
@@ -35,13 +28,14 @@ x_grid=np.arange(min(X), max(X),0.1)
 x_grid=x_grid.reshape((len(x_grid),1))
 
 plt.scatter(X,y, color='red')
-plt.plot(X, regressor.predict(X), color='blue')
-plt.title("Truth or Bluff (Decison Tree Regression)")
+plt.plot(x_grid,regressor.predict(X) , color='blue')
+plt.title("Truth or Bluff (Random Forest Regression)")
 plt.xlabel('Position level')
 plt.ylabel("Salary")
 plt.show()
 
 
-
+#Predicting a new result with Polynomial resgression
+y_predict=regressor.predict(6.5)
 
 
